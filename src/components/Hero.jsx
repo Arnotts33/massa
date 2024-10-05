bde538d16588b6053102bbe2e94e31582e77c38c
Arnotts33 <arnaudlht@gmail.com>
2024-10-05 11:48:58 +0200

    add hero text section and images to asset folder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,19 @@ function Hero() {
 					alt="massa fridge"
 				/>
 			</div>
+
+			<div className={styles.hero__content}>
+				<div className={styles.hero__text}>
+					<p>
+						Massa, your go-to spot for fresh Mediterranean street
+						food, homemade falafels, creamy hummus, and warm pita.
+						Located in the vibrant center of Bordeaux. Crafted daily
+						with passion and authentic ingredients, our dishes bring
+						the bold flavors of the Middle East to you in a
+						welcoming, urban atmosphere.
+					</p>
+				</div>
+			</div>
 		</section>
 	);
 }
