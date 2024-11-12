c44400ccc64cf62eab621f4f43f6a60b01cc22df
Arnotts33 <arnaudlht@gmail.com>
2024-11-12 21:57:38 +0100

    Added toggle button functionnality in Menu component to toggle between food and drinks menus

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import Drinks from "../ui/Drinks";
 import Food from "../ui/Food";
 import Footer from "../ui/Footer";
@@ -5,6 +6,30 @@ import Navbar from "../ui/Navbar";
 import styles from "./Menu.module.css";
 
 function Menu() {
+	const [isFoodMenuToggle, setIsFoodMenuToggle] = useState(true);
+	const [isDrinksMenuToggle, setIsDrinksMenuToggle] = useState(false);
+
+	const foodButtonRef = useRef(null);
+	const drinksButtonRef = useRef(null);
+
+	const handleFoodMenuToggle = () => {
+		setIsFoodMenuToggle(true);
+		setIsDrinksMenuToggle(false);
+		let foodButton = foodButtonRef.current;
+		let drinksButton = drinksButtonRef.current;
+		foodButton.classList.add(styles.active);
+		drinksButton.classList.remove(styles.active);
+	};
+
+	const handleDrinksMenuToggle = () => {
+		setIsFoodMenuToggle(false);
+		setIsDrinksMenuToggle(true);
+		let foodButton = foodButtonRef.current;
+		let drinksButton = drinksButtonRef.current;
+		drinksButton.classList.add(styles.active);
+		foodButton.classList.remove(styles.active);
+	};
+
 	return (
 		<>
 			<Navbar />
@@ -19,15 +44,26 @@ function Menu() {
 				</section>
 				<section className={styles.menu_selection__section}>
 					<div className={styles.food_menu}>
-						<button className={styles.active}>FOOD</button>
+						<button
+							ref={foodButtonRef}
+							className={styles.active}
+							onClick={handleFoodMenuToggle}
+						>
+							FOOD
+						</button>
 					</div>
 					<div className={styles.drinks_menu}>
-						<button>DRINKS</button>
+						<button
+							ref={drinksButtonRef}
+							onClick={handleDrinksMenuToggle}
+						>
+							DRINKS
+						</button>
 					</div>
 				</section>
 				<section className={styles.menu__section}>
-					<Food />
-					{/* <Drinks /> */}
+					{isFoodMenuToggle && <Food />}
+					{isDrinksMenuToggle && <Drinks />}
 				</section>
 			</main>
 			<Footer />
